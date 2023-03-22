1b5b13bd7dca8250da48e4f6d575828f86f22bb8
joaoVictorS <jonnymcz2@gmail.com>
2023-03-22 17:58:59 -0300

    add footer component and contact component in last section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { ContactForm } from "@/components/ContatForm";
+import { Footer } from "@/components/Footer";
 import { LandingNavigator } from "@/components/navigators/LandingNavigator";
 import styles from "./LandingPage.module.scss";
 
@@ -5,6 +7,9 @@ export default function LandingPage() {
   return (
     <main className={styles.main}>
       <LandingNavigator />
+      <ContactForm />
+      <Footer />
+
     </main>
   );
 }
