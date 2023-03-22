e8cc244d7cbf97d696427c17211a52fb38abe146
joaoVictorS <jonnymcz2@gmail.com>
2023-03-22 14:25:40 -0300

    add nav bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import { Roboto } from "next/font/google";
+import type { AppProps } from "next/app";
+
+// Using variable to be easily accessible when styling
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-roboto",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className={`${roboto.variable}`}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
